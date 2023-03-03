d3586fc19790f76fb88b8e3d0dbb3b1ae5b08dd4
Adrian Aguero <iceberg.04@gmail.com>
2023-03-03 00:55:46 -0300

    Update readme

--- README.rb
+++ README.rb
@@ -1,48 +1,49 @@
-Belt Exam (MERN)
-Date: 26 Feb 2023
-Author: Adrian Aguero
+# Membership Management System
 
-Instalacion
+## Description
+This is a Membership Management System designed to help organizations manage their membership, quotas, payments, and other related activities. This system is built to provide an easy-to-use interface for administrators to manage their members information and activities.
 
-database:
-mongodb installed
+## Features
+* **Member Management:** Add, update, and delete member information including name, contact details, and membership status.
+Membership Quota Management: Define membership quotas for different types of memberships, such as monthly, annual, or lifetime. Set prices and expiration dates for each quota.
 
-server:
-cd kanbanboard
-npm install
-npm start
+* **Payment Management:** Record and track member payments for their respective quotas. Provide various payment options, such as online payment, bank transfer, or cash.
 
-client:
-cd kanbanboard
-cd client
-npm install
-npm start
+* **Renewal Management:** Automatically generate renewal reminders for expiring memberships and provide options for renewing membership.
+Reporting: Generate reports on member activities, payment status, and membership trends. Export reports in various formats such as CSV or PDF.
 
-Open browser in http://localhost:3000
+## Purpose
+This project is a personal initiative for the Coding Dojo Bootcamp. It aims to provide a practical application of the skills learned during the bootcamp, particularly in the areas of web development, databases, and project management. By building this membership management system, we hope to gain a deeper understanding of the technologies involved, as well as to develop a portfolio piece that can showcase our skills to potential employers.
 
-* Puntos desarrollados *
-* muestra todos los proyectos en tres columnas según el estado del proyecto. HECHO
-* el botón "Iniciar proyecto" ("start project") debe establecer el estado del proyecto en "En curso". HECHO
-* el botón "Mover a Completado"  ("Move to completed") debe establecer el estado del proyecto en "Completado". HECHO
-* el botón "Eliminar proyecto" ("Remove Project") debería eliminar el proyecto. HECHO
-* Validaciones, el proyecto es obligatorio y debe tener 3 caracteres o más. HECHO
-* Validaciones, se requiere la fecha de vencimiento. HECHO
+## Getting Started
+### Prerequisites
+To run this project, you need to have the following software installed on your system:
 
-Para lograr el cinturón negro, se realizo:
-* Ordenar los proyectos por "Fecha de vencimiento". HECHO
-* Indica si el proyecto está vencido (la fecha de vencimiento es anterior a la de hoy) HECHO pero sin usar moments.js
-* Asegurarse de que el proyecto sea único al agregarlo a la base de datos. HECHO
+* Node.js
+* MongoDB
 
-Guía/Requisitos generales del proyecto:
-Login y Registro con validaciones
-El usuario puede [crear] objeto a la base de datos
-El usuario puede [leer] objetos creados de la base de datos
-El usuario puede hacer [me gusta], añadir a [favoritos], responder, etc.
-El usuario puede [editar] el perfil o los datos publicados/creados
-El usuario puede [eliminar] el perfil o los datos publicados/creados
-Uso del motor Node/Express y React en el lado del cliente.
-La app incluye rutas protegidas (el usuario debe tener la sesión activa para acceder a las rutas de la app)
-La aplicación incluye contenido estático (CSS, imágenes, videos, audio, etc)
-Los datos creados deben estar validados (no deben existir datos duplicados, la creación de información debe incluir contenido, etc)
-La app es responsive
-Sugiero que intenten incluir un mecanismo de chat en su app. No es un requisito obligatorio, pero sería genial incluir esta característica en los proyectos.
+### Installing
+1. Clone the repository or download the source code
+2. Install the required dependencies using the command npm install or yarn install
+3. Create a .env file in the root directory and fill in the required configuration variables
+4. Start the server using the command npm start or yarn start
+
+### Running the tests
+To run the automated tests for this system, use the command npm test or yarn test.
+
+### Built With
+* Backend: Node.js, Express.js, MongoDB
+* Frontend: React.js, Bootstrap, HTML/CSS
+
+## Contributing
+We welcome contributions from the community to improve this system. To contribute, please follow these guidelines:
+
+Fork the repository and create a new branch for your feature or bug fix.
+Make your changes and submit a pull request to the main branch.
+Ensure that your code is well-documented and follows the project's coding standards.
+Provide a detailed description of your changes in the pull request.
+License
+This project is licensed under the MIT License.
+
+## Acknowledgments
+This project is part of the Coding Dojo Bootcamp curriculum.
